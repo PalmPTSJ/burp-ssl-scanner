7f9e84ce6a2cb23a2477cc2dfef6a873bfb49ff6
Meatasit Karakate <khem_inw@protonmail.ch>
2018-07-09 16:57:09 +0700

    more UI implementations

--- burp-ssl-scanner.py
+++ burp-ssl-scanner.py
@@ -7,15 +7,12 @@ try:
     from burp import IContextMenuFactory
     from burp import IContextMenuInvocation
     from burp import IHttpRequestResponse
-    # from burp import IScanIssue
-    # from array import array
-    # from time import sleep
-    from java.io import PrintWriter
+    from java.io import PrintWriter, File, FileWriter
     from java.lang import Runnable
     from javax.swing import (JTable, JScrollPane, JSplitPane, JButton, JPanel,
                              JTextField, JLabel, SwingConstants, JDialog, Box,
                              JCheckBox, JMenuItem, SwingUtilities, JOptionPane,
-                             BoxLayout, JPopupMenu)
+                             BoxLayout, JPopupMenu, JFileChooser)
 
     from javax.swing.border import EmptyBorder
     from javax.swing.table import AbstractTableModel
@@ -30,14 +27,11 @@ try:
     import socket
     from java.lang import System
 
-    #sys.path.append(os.path.dirname(os.path.realpath('testselenium.jar')) + '/testselenium.jar')
-
-    #from testselenium import Test
-
     import re
     import hashlib
 
     import ssl
+    import time
 
     import result
     import connection_test
@@ -63,6 +57,10 @@ class BurpExtender(IBurpExtender, ITab):
         # self._callbacks.registerExtensionStateListener(self)
         self._helpers = callbacks.getHelpers()
 
+        # initialize the main scanning event and thread
+        self.scanningEvent = Event()
+        self.scannerThread = None
+
         # main split pane
         self._splitpane = JSplitPane(JSplitPane.VERTICAL_SPLIT)
         self._splitpane.setBorder(EmptyBorder(20, 20, 20, 20))
@@ -87,14 +85,14 @@ class BurpExtender(IBurpExtender, ITab):
         self._topPanel.add(self.setupPanel, BorderLayout.PAGE_START)
         
         # Status bar
-        self.crawlStatusPanel = JPanel(FlowLayout(FlowLayout.LEADING, 10, 10))
+        self.scanStatusPanel = JPanel(FlowLayout(FlowLayout.LEADING, 10, 10))
 
-        self.crawlStatusPanel.add(JLabel("Status: ", SwingConstants.LEFT))
+        self.scanStatusPanel.add(JLabel("Status: ", SwingConstants.LEFT))
 
-        self.crawlStatusLabel = JLabel("Ready to scan", SwingConstants.LEFT)
-        self.crawlStatusPanel.add(self.crawlStatusLabel)
+        self.scanStatusLabel = JLabel("Ready to scan", SwingConstants.LEFT)
+        self.scanStatusPanel.add(self.scanStatusLabel)
 
-        self._topPanel.add(self.crawlStatusPanel, BorderLayout.LINE_START)
+        self._topPanel.add(self.scanStatusPanel, BorderLayout.LINE_START)
 
         self._splitpane.setTopComponent(self._topPanel)
 
@@ -104,7 +102,7 @@ class BurpExtender(IBurpExtender, ITab):
 
         self.textEditorInstance = callbacks.createTextEditor()
         self.textEditorInstance.setEditable(False)
-        initialText = 'Press "Start scanning" to get started'
+        initialText = 'Press "Start scanning" to get started.'
         self.textEditorInstance.setText(self._helpers.stringToBytes(initialText))
         self._bottomPanel.add(self.textEditorInstance.getComponent(), BorderLayout.CENTER)
 
@@ -118,8 +116,12 @@ class BurpExtender(IBurpExtender, ITab):
 
         callbacks.addSuiteTab(self)
         
-        print "Burp SSL Explorer loaded"
-        
+        print "Burp SSL Scanner loaded"
+
+        self.scannerMenu = ScannerMenu(self)
+        callbacks.registerContextMenuFactory(self.scannerMenu)
+        print "SSL Scanner custom menu loaded"
+
         #print os.popen("openssl version").read()
 
         print 'Done'
@@ -129,29 +131,58 @@ class BurpExtender(IBurpExtender, ITab):
     def startScan(self, ev) :
 
         host = self.hostField.text
+        self.scanningEvent.set()
         if(len(host) == 0):
             return 
 
-        try:
-            print("Start scanning "+host)
+        # try:
+        print("Start scanning "+host)
+        self.scannerThread = Thread(target=self.scan, args=(host, ))
+        self.scannerThread.start()
 
-            res = result.Result()
+        # except:
+        #     print("Something wrong")
 
+    def scan(self, host):
+        res = result.Result()
+        try :
+            SwingUtilities.invokeLater(
+                ScannerRunnable(self.scanStatusLabel.setText, 
+                                ("Checking for supported SSL/TLS versions",)))
             con = connection_test.ConnectionTest(res, host, 443)
             con.start()
 
+            SwingUtilities.invokeLater(
+                ScannerRunnable(self.scanStatusLabel.setText, 
+                                ("Checking for Heartbleed",)))
             heartbleed = heartbleed_test.HeartbleedTest(res, host, 443)
             heartbleed.start()
 
+            SwingUtilities.invokeLater(
+                ScannerRunnable(self.scanStatusLabel.setText, 
+                                ("Checking for CCS Injection",)))
             ccs = ccs_test.CCSTest(res, host, 443)
             ccs.start()
-        except:
-            print("Something wrong")
-        
+        except BaseException as e :
+            SwingUtilities.invokeLater(
+                ScannerRunnable(self.scanStatusLabel.setText, 
+                                ("An error occurred. Please refer to the errors tab for more information.",)))
+            time.sleep(1)
+            print(e)
+        self.scanningEvent.clear()
+        SwingUtilities.invokeLater(
+                ScannerRunnable(self.scanStatusLabel.setText, 
+                                ("Ready to scan",)))
         print("Finished scanning")
 
-    def saveToFile(self):
-        print "Saved"
+    def saveToFile(self, event):
+        fileChooser = JFileChooser()
+        if (fileChooser.showSaveDialog(self.getUiComponent()) == JFileChooser.APPROVE_OPTION):
+            fw = FileWriter(fileChooser.getSelectedFile())
+            fw.write(self._helpers.bytesToString(self.textEditorInstance.getText()))
+            fw.flush()
+            fw.close()
+            print "Saved"
 
     def getTabCaption(self):
         return "SSL Scanner"
@@ -159,3 +190,39 @@ class BurpExtender(IBurpExtender, ITab):
     def getUiComponent(self):
         return self._splitpane
     
+class ScannerMenu(IContextMenuFactory):
+    def __init__(self, scannerInstance):
+        self.scannerInstance = scannerInstance
+
+    def createMenuItems(self, contextMenuInvocation):
+        self.contextMenuInvocation = contextMenuInvocation
+        sendToSSLScanner = JMenuItem(
+            "Send URL to SSL Scanner", actionPerformed=self.getSentUrl)
+        menuItems = ArrayList()
+        menuItems.add(sendToSSLScanner)
+        return menuItems
+
+    def getSentUrl(self, event):
+        for selectedMessage in self.contextMenuInvocation.getSelectedMessages():
+            if (selectedMessage.getHttpService() != None):
+                try:
+                    url = self.scannerInstance._helpers.analyzeRequest(
+                        selectedMessage.getHttpService(),
+                        selectedMessage.getRequest()).getUrl()
+                    print "URL: " + url.toString()
+                    self.scannerInstance.hostField.setText(url.toString())
+                except:
+                    self.scannerInstance._callbacks.issueAlert(
+                        "Cannot get URL from the currently selected message " +
+                        str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]))
+            else:
+                self.scannerInstance._callbacks.issueAlert(
+                    "The selected request is null.")
+
+class ScannerRunnable(Runnable):
+    def __init__(self, func, args):
+        self.func = func
+        self.args = args
+
+    def run(self):
+        self.func(*self.args)
